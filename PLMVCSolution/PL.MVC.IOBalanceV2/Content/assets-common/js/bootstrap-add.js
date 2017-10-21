b77f72b1fc80c6cf0393a7d861133c4771397c91
xawileones <xawileones@gmail.com>
2017-10-21 20:47:33 +0800

    UI Updates: Responsive Menu

--- PLMVCSolution/PL.MVC.IOBalanceV2/Content/assets-common/js/bootstrap-add.js
+++ PLMVCSolution/PL.MVC.IOBalanceV2/Content/assets-common/js/bootstrap-add.js
@@ -1,5 +1,5 @@
 $(function () {
-   
+
     // Custom Collapse for Submenus
     $('*[data-toggle="accordion-menu"]').each(function () {
         var $this = $(this);
@@ -21,7 +21,7 @@ $(function () {
 
         });
     });
-   
+
 
     // Build - Show and Hide Loader
     var loader_dom = '<div class="loader-mask"><div class="loader-container"><div class="loader-cell"><i class="md md-2x md-spin md-settings"></i></div></div></div>';
@@ -36,12 +36,62 @@ $(function () {
         subMenu = $('.sub-menu');
     activeLink.closest('li').addClass('active');
     for (var i = 0; i < subMenu.length; i++) {
-       var hasActive = $(subMenu[i]).find('.active').length ? true : false;
+        var hasActive = $(subMenu[i]).find('.active').length ? true : false;
         if (hasActive) {
             $(subMenu[i]).addClass('in').removeClass('collapse');
         }
     }
+    $('#mainNavbar').mCustomScrollbar({
+        autoHideScrollbar: false,
+        axis: 'y',
+        contentTouchScroll: true,
+        scrollbarPosition: 'inside',
+        scrollInertia: 500,
+        scrollButtons: { enable: false },
+        theme: 'light',
+        live: true,
+        liveSelector: '[custom-scroll]',
+        advanced: { updateOnContentResize: true },
+    });
+    $('.navbar-toggle').on('click', function () {
+        var mainNavbar = $('#mainNavbar');
 
+        if (mainNavbar.hasClass('showed')) {
+            mainNavbar.removeClass('showed');
+            mainNavbar.velocity({
+                translateX: '-100%'
+            })
+        }
+        else {
+            mainNavbar.addClass('showed');
+            mainNavbar.velocity({
+                translateX: '0'
+            })
+        }
+    });
+
+    var resizeId;
+    $(window).resize(function () {
+        clearTimeout(resizeId);
+        resizeId = setTimeout(doneResizing, 500);
+    });
+    function doneResizing() {
+        var win = $(this);
+        var mainNavbar = $('#mainNavbar');
+        if (win.width() <= 768) {
+            mainNavbar.removeClass('showed');
+            mainNavbar.velocity({
+                translateX: '-100%'
+            });
+        }
+        else {
+            mainNavbar.removeClass('showed');
+            mainNavbar.velocity({
+                translateX: '0'
+            })
+        }
+    }
+    doneResizing();
 });
 
 
